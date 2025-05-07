6a2b731341dbf350a119381572e18e6fc3d04f08
CatalpaEel <139088727+CatalpaEel@users.noreply.github.com>
2025-05-07 15:29:02 +0800

    Change to jimeng api and generate reference prompt

--- image.py
+++ image.py
@@ -1,5 +1,6 @@
 import base64
 import time
+import json
 
 from volcengine.visual.VisualService import VisualService
 from openai import OpenAI
@@ -16,13 +17,13 @@ class ImageGenerator:
         self.visual_service.set_sk(doubao_secret_key)
         
     def generate_image(self, prompt, path, width=1024, height=768):
-        target = [(512,512), (384,512), (512,341), (341,512), (512,288), (288,512)] # 1:1, 4:3, 3:4, 3:2, 2:3, 16:9, 9:16
-        s_width, s_height = min(target, key=lambda x: abs(width/height - x[0]/x[1])) # Find the closest preset size to make the model happy
+        # target = [(512,512), (384,512), (512,341), (341,512), (512,288), (288,512)] # 1:1, 4:3, 3:4, 3:2, 2:3, 16:9, 9:16
+        # s_width, s_height = min(target, key=lambda x: abs(width/height - x[0]/x[1])) # Find the closest preset size to make the model happy
         form = {
-            "req_key": "high_aes_general_v21_L",
+            "req_key": "jimeng_high_aes_general_v21_L",
             "prompt": prompt,
-            "width": s_width,
-            "height": s_height,
+            "width": width,
+            "height": height,
         }
         response = self.visual_service.cv_process(form)
         image = base64.b64decode(response['data']['binary_data_base64'][0])
@@ -40,19 +41,15 @@ class ImagePrompter:
         self.client = OpenAI(api_key=deepseek_api_key, base_url="https://api.deepseek.com")
         self.system_prompt = """
         你是一位大模型提示词生成专家。学生会将要举办一次活动，请根据用户的需求编写一个文生图模型的提示词，来指导大模型进行活动宣传海报的生成。
-        用户会给出需求。除提示词外不要添加任何其他内容，不要捏造输入之外的信息。
-        提示词模版：
-        - 活动主题：简要叙述
-        - 文字排版描述：文字内容放在双引号“”内，描述文字的位置、大小、颜色和风格，精准调整排版效果。文字不宜过多，只需要最主要的信息
-        - 背景描述：海报背景的配色、元素、组合、排版，尽量简洁简约
-        - 艺术风格：确定画面整体基调，调节画面整体排版与配色
+        用户会给出需求。不要出现“北京大学”等现实中的名称。
+        下面是一些提示词模板，你需要仿照其风格：
         """
         
-    def generate_prompt(self, input):
+    def generate_prompt(self, input, reference):
         response = self.client.chat.completions.create(
             model="deepseek-chat",
             messages=[
-                {"role": "system", "content": self.system_prompt},
+                {"role": "system", "content": self.system_prompt + reference},
                 {"role": "user", "content": input},
             ],
             stream=False
@@ -67,12 +64,16 @@ if __name__ == "__main__":
     prompter = ImagePrompter()
     painter = ImageGenerator()
 
-    input_text = "{'type': '比赛', 'subject': 'AI与大模型'}"
+    input_text = "AI与大模型比赛"
 
-    with open("../image_reference/summarize", "r", encoding='utf-8') as f:
-        reference = f.read()
-
-    prompt = prompter.generate_prompt(input_text)
+    with open("../image_reference/img_ref.json", "r", encoding='utf-8') as f:
+        ref_json = json.loads(f.read())
     
+    reference = ""
+    for ref in ref_json:
+        reference += ref["name"] + ': ' + ref["prompt"] + '\n'
+    
+    prompt = prompter.generate_prompt(input_text, reference)
+
     path = f'../debug/image_{int(time.time())}.png'
     image = painter.generate_image(prompt=prompt, path=path, width=1024, height=768)
--- image_reference.py
+++ image_reference.py
@@ -2,6 +2,7 @@ import base64
 import os
 import glob
 import sys
+import json
 
 from openai import OpenAI
 from api_key import doubao_api_key
@@ -13,13 +14,7 @@ class ImageAnalyst:
     def __init__(self):
         self.client = OpenAI(api_key=doubao_api_key, base_url="https://ark.cn-beijing.volces.com/api/v3")
         self.system_prompt = """
-            你是一个图片分析专家，你需要根据用户给出的图片以及图片的标题，分析给出的图片的风格、排版等特征，输出格式以下内容：
-            - 图片主题
-            - 图片的文字以及对应的位置、颜色、字体，可能有多块文字
-            - 图片背景的配色、元素、组合
-            - 图片艺术风格
-            - 图片排版
-            - 图片配色
+            请根据图片反推详细的中文文生图提示词，不要输出提示词外的多余信息
         """
 
     def encode_image(self, image_path):
@@ -38,10 +33,6 @@ class ImageAnalyst:
                             "type": "image_url",
                             "image_url": { "url":  f"data:image/png;base64,{self.encode_image(image_path)}" },
                         },
-                        {
-                            "type": "text",
-                            "text": os.path.basename(image_path),
-                        }
                     ],
                 }
             ],
@@ -49,35 +40,6 @@ class ImageAnalyst:
             stream = False
         )
         return response.choices[0].message.content
-
-
-class ImageSummarizer:
-    """
-    Summarize the feature of the image
-    """
-    def __init__(self):
-        self.client = OpenAI(api_key=doubao_api_key, base_url="https://ark.cn-beijing.volces.com/api/v3")
-        self.system_prompt = """
-            你是一个图片特征分析专家，你需要总结用户给出的信息，分析这些图片的共性特征，比如艺术风格、排版形式等等。
-            直接罗列共性特征，不要输出多余信息。
-        """
-
-    def encode_image(self, image_path):
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode('utf-8')
-
-    def summarize_image(self, input):
-        response = self.client.chat.completions.create(
-            model = "doubao-1-5-vision-pro-32k-250115",
-            messages = [
-                { "role": "system", "content": self.system_prompt },
-                { "role": "user", "content": input }
-            ],
-            response_format = { "type": "text" },
-            stream = False
-        )
-        return response.choices[0].message.content
-        
 
 
 if __name__ == "__main__":
@@ -91,18 +53,13 @@ if __name__ == "__main__":
 
     image_analyst_agent = ImageAnalyst()
     
-    results = ""
+    results = []
 
     for image_path in image_paths:
-        name = os.path.basename(image_path)
-        name = os.path.splitext(name)[0]
+        name = os.path.splitext(os.path.basename(image_path))[0]
         result = image_analyst_agent.analyze_image(image_path)
-        results += result
+        results.append({"name":name, "prompt":result})
         print(f"{name}分析完毕：\n{result}\n")
-        with open(f'../image_reference/{name}', 'w', encoding='utf-8') as f:
-            print(result, file=f)
 
-    summarize_agent = ImageSummarizer()
-    result = summarize_agent.summarize_image(results)
-    with open(f'../image_reference/summarize', 'w', encoding='utf-8') as f:
-        print("总结：\n" + result, file=f)
+    with open(f"../image_reference/img_ref.json", "w", encoding='utf-8') as f:
+        json.dump(results, f, indent=4, ensure_ascii=False)
